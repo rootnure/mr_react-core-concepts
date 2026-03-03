178a6713813038284c0c1793baf3bd05db40fdc8
rootnure <nur.diu.2791@gmail.com>
2026-03-03 14:26:08 +0600

    Event

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,39 @@ import ToDo from './Todo'
 import User from './User';
 
 function App() {
+  function handleClick() {
+    console.log('clicked');
+  }
+
+  const handleClick3  = ()=> {
+    console.log("clicked 3");
+  }
+
+  const handleClick5 = num => {
+    console.log(num);
+  }
+
+
   // const firends = ['Asif', 'Babul', 'Chandan', 'Rahul', 'Ripon'];
 
-  const users = [
-    {id: 1, name: 'Habib', age: 29},
-    {id: 2, name: 'Hasan', age: 33},
-    {id: 3, name: 'Sarwar', age: 24}
-  ]
+  // const users = [
+  //   {id: 1, name: 'Habib', age: 29},
+  //   {id: 2, name: 'Hasan', age: 33},
+  //   {id: 3, name: 'Sarwar', age: 24}
+  // ]
 
   return (
     <>
-      <h1>React Core Concepts</h1>
+      <h1>React Core Concepts Part 2</h1>
+      <button onClick={handleClick}>Click Me</button>
+      <button onClick={function (){console.log("clicked 2")}}>Click Me 2</button>
+      <button onClick={handleClick3}>Click Me 3</button>
+      <button onClick={() => console.log('clicked 4')}>Click Me 4</button>
+      <button onClick={() => handleClick5(5)}>Click Me 5</button>
 
-      {
+      {/* {
         users.map(user => <User key={user.id} user={user}></User>)
-      }
+      } */}
 
     {/* {
       firends.map((friend, idx) => <Friend key={idx} friend={friend}></Friend>)
