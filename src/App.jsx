b8df1ec5046dd8e4840981e4ed24017f62499189
rootnure <nur.diu.2791@gmail.com>
2026-03-03 12:40:24 +0600

    JSX, Components, Props, Props Destructuring, Unidirectional

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>React Core Concepts</h1>
+      <Student name='Shamim' age={28}></Student>
+      <Sports></Sports>
+      <Person></Person>
+      <Student name='Habib' age={25}></Student>
+      <Student name='Hasan'></Student>
     </>
+  )
+}
+
+function Student({name, age=18}) {
+  return (
+    <div className="student">
+      <p>Name: {name}</p>
+      <p>Age: {age}</p>
+    </div>
+  )
+}
+
+function Sports() {
+  const sportsStyle = {
+    color: "red",
+    backgroundColor: 'yellow'
+  }
+  return (
+    <div style={sportsStyle}>
+      <h2>Let's play</h2>
+      <ul>
+        <li>Asif</li>
+        <li>Rabbi</li>
+        <li>Habib</li>
+      </ul>
+    </div>
+  )
+}
+
+function Person () {
+  const age = 17;
+  const name = 'Jahidul';
+  return (
+    <p style={{backgroundColor: "red"}}>I am {name}. Age: {age}</p>
   )
 }
 
